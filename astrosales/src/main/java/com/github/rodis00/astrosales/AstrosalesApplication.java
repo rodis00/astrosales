6bf056098abbdce6077583977059edf76549820d
rodis00 <happyekkov@gmail.com>
2023-11-05 17:12:28 +0100

    init empty backend project, add db_schema png

---
+++ astrosales/src/main/java/com/github/rodis00/astrosales/AstrosalesApplication.java
@@ -0,0 +1,13 @@
+package com.github.rodis00.astrosales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrosalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrosalesApplication.class, args);
+	}
+
+}
