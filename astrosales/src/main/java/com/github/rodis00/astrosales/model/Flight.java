c98a8dbcb5d421cf4cc7c571984c5d3a0aee7f9b
rodis00 <happyekkov@gmail.com>
2023-11-08 14:19:01 +0100

    add flight entity

---
+++ astrosales/src/main/java/com/github/rodis00/astrosales/model/Flight.java
@@ -0,0 +1,25 @@
+package com.github.rodis00.astrosales.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "flight")
+public class Flight {
+    @Id
+    @GeneratedValue
+    private int id;
+    private LocalDateTime dateOfFlight;
+    private String startingPlace;
+    private String landingPlace;
+    private float ticketPrice;
+    private float ticketPriceVip;
+    private int availabilityOfPlaces;
+
+}
