a3cb98e43d415a96112ab450efe93511979661d8
rodis00 <happyekkov@gmail.com>
2023-11-13 22:16:15 +0100

    add Flight utils for no available places

---
+++ astrosales/src/main/java/com/github/rodis00/astrosales/utils/FlightUtils.java
@@ -0,0 +1,6 @@
+package com.github.rodis00.astrosales.utils;
+
+public class FlightUtils {
+    public static final String NO_AVAILABLE_PLACES_CODE = "003";
+    public static final String NO_AVAILABLE_PLACES_MESSAGE = "No available places.";
+}
