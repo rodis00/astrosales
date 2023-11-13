3002693da92f70c8ba64f8f3bef4668f1e7c5349
rodis00 <happyekkov@gmail.com>
2023-11-13 15:30:08 +0100

    add user utils

---
+++ astrosales/src/main/java/com/github/rodis00/astrosales/utils/UserUtils.java
@@ -0,0 +1,6 @@
+package com.github.rodis00.astrosales.utils;
+
+public class UserUtils {
+    public static final String USER_EXISTS_CODE = "002";
+    public static final String USER_EXISTS_MESSAGE = "User with this email already exists.";
+}
