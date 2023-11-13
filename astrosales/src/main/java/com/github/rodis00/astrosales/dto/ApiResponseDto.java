8c554c1fbd460c7125f1bf2af11194c22d65f6eb
rodis00 <happyekkov@gmail.com>
2023-11-13 15:29:31 +0100

    add constructors, delete unnecessary exception

--- astrosales/src/main/java/com/github/rodis00/astrosales/dto/ApiResponseDto.java
+++ astrosales/src/main/java/com/github/rodis00/astrosales/dto/ApiResponseDto.java
@@ -1,8 +1,12 @@
 package com.github.rodis00.astrosales.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponseDto {
     private String responseCode;
     private String responseMessage;
