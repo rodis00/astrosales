bdd11ac4b567cff452f010b79129914f9e055f9c
rodis00 <happyekkov@gmail.com>
2023-11-15 11:30:00 +0100

    add new flight utils

--- astrosales/src/main/java/com/github/rodis00/astrosales/utils/FlightUtils.java
+++ astrosales/src/main/java/com/github/rodis00/astrosales/utils/FlightUtils.java
@@ -3,4 +3,7 @@ package com.github.rodis00.astrosales.utils;
 public class FlightUtils {
     public static final String NO_AVAILABLE_PLACES_CODE = "003";
     public static final String NO_AVAILABLE_PLACES_MESSAGE = "No available places.";
+
+    public static final String FLIGHT_DATE_IN_THE_PAST_CODE = "004";
+    public static final String FLIGHT_DATE_IN_THE_PAST_MESSAGE = "Flight date is in the past.";
 }
