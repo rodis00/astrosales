40923b6acab816ac86cd08e20800b5d8b47ae70e
rodis00 <happyekkov@gmail.com>
2023-11-20 17:43:26 +0100

    add role field

--- astrosales/src/main/java/com/github/rodis00/astrosales/model/User.java
+++ astrosales/src/main/java/com/github/rodis00/astrosales/model/User.java
@@ -2,21 +2,64 @@ package com.github.rodis00.astrosales.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
     @Column(unique = true)
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
     @OneToOne(
             cascade = CascadeType.ALL,
             orphanRemoval = true
     )
     @JoinColumn(name = "user_profile_id")
     private UserProfile userProfile;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
