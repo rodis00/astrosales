4e120c3d3dfc6890cae3ffa1b4331e0f3edd44d8
rodis00 <happyekkov@gmail.com>
2023-12-16 17:59:33 +0100

    add routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login-page', component: LoginPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
 ];
--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
-export class LoginPageComponent {
-
-}
+export class LoginPageComponent {}
