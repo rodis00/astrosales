792f57219f711110595e93fae7088af4285061d5
rodis00 <happyekkov@gmail.com>
2023-12-19 18:13:16 +0100

    add route for flight details

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { FlightsComponent } from './flights/flights.component';
+import { FlightComponent } from './flight/flight.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'flights', component: FlightsComponent },
+  { path: 'flight/:id', component: FlightComponent },
 ];
