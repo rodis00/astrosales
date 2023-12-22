52eb32e16f51196172c39ea2464eb809cbbb61c7
rodis00 <happyekkov@gmail.com>
2023-12-22 22:00:09 +0100

    add auth service

---
+++ frontend/src/app/services/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl: string = 'http://localhost:8080/astrosales/api/v1';
+
+  constructor(private http: HttpClient) {}
+
+  public authenticate(user: User): Observable<string> {
+    return this.http.post<string>(`${this.apiUrl}/auth/authenticate`, user);
+  }
+}
