04f35b4a80163754a696e764a4bab05819f8f74b
rodis00 <happyekkov@gmail.com>
2023-12-25 16:10:29 +0100

    refactor auth register controller in the api

--- astrosales/src/main/java/com/github/rodis00/astrosales/auth/AuthenticationController.java
+++ astrosales/src/main/java/com/github/rodis00/astrosales/auth/AuthenticationController.java
@@ -1,6 +1,10 @@
 package com.github.rodis00.astrosales.auth;
 
+import com.github.rodis00.astrosales.dto.ApiResponseDto;
+import com.github.rodis00.astrosales.service.UserService;
+import com.github.rodis00.astrosales.utils.UserUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("astrosales/api/v1/auth")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final UserService userService;
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        if (!userService.existsByEmail(request.getEmail()))
+            return ResponseEntity.ok(authenticationService.register(request));
+        else {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponseDto(
+                        UserUtils.USER_EXISTS_CODE,
+                        UserUtils.USER_EXISTS_MESSAGE
+                    ));
+        }
     }
 
     @PostMapping("/register-admin-user")
