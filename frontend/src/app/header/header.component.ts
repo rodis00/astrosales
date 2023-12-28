b0102f25c28db389871580969afd8fbf8cd06480
rodis00 <happyekkov@gmail.com>
2023-12-28 21:24:16 +0100

    add jwtHelper to packages

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TokenService } from '../services/token/token.service';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  providers: [TokenService],
 })
 export class HeaderComponent {
   public token: any = sessionStorage.getItem('token');
+
+  userDetails: any = {};
+
+  constructor(private tokenService: TokenService) {
+    this.userDetails = tokenService.getRolesAndIdFromToken(this.token);
+    console.log(this.userDetails);
+  }
 
   public logout(): void {
     sessionStorage.removeItem('token');
