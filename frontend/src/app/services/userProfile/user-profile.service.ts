ace7a997bc8acd8fb90278cd9ecd014d08abf17d
rodis00 <happyekkov@gmail.com>
2024-01-01 23:12:54 +0100

    change permission access

--- frontend/src/app/services/userProfile/user-profile.service.ts
+++ frontend/src/app/services/userProfile/user-profile.service.ts
@@ -9,9 +9,10 @@ import { Observable } from 'rxjs';
 export class UserProfileService {
   private apiUrl: string = 'http://localhost:8080/astrosales/api/v1';
   private token = sessionStorage.getItem('token');
-
+  private tmpToken: string =
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJ1c2VySWQiOjEsInJvbGVzIjpbeyJhdXRob3JpdHkiOiJBRE1JTiJ9XSwiaWF0IjoxNzA0MTQ1Mjc3LCJleHAiOjE3MDQxNDY3MTd9.Et_0TfcIF_xp7WM0LRFoip7eZ6MtUwxjh0ZJ-oVyPGs';
   private headers = new HttpHeaders({
-    Authorization: `Bearer ${this.token}`,
+    Authorization: `Bearer ${this.tmpToken}`,
   });
 
   private options = {
@@ -22,9 +23,6 @@ export class UserProfileService {
 
   public getUserProfile(id: number): Observable<UserProfile> {
     console.log(this.options);
-    return this.http.get<UserProfile>(
-      `${this.apiUrl}/user-profiles/${id}`,
-      this.options
-    );
+    return this.http.get<UserProfile>(`${this.apiUrl}/user-profiles/${id}`);
   }
 }
