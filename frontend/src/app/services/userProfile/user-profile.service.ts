56f1edb80d3beb0348803b5fa892500dcdefd13d
rodis00 <happyekkov@gmail.com>
2024-01-02 17:59:06 +0100

    edit user-profile fetch

--- frontend/src/app/services/userProfile/user-profile.service.ts
+++ frontend/src/app/services/userProfile/user-profile.service.ts
@@ -9,10 +9,9 @@ import { Observable } from 'rxjs';
 export class UserProfileService {
   private apiUrl: string = 'http://localhost:8080/astrosales/api/v1';
   private token = sessionStorage.getItem('token');
-  private tmpToken: string =
-    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJ1c2VySWQiOjEsInJvbGVzIjpbeyJhdXRob3JpdHkiOiJBRE1JTiJ9XSwiaWF0IjoxNzA0MTQ1Mjc3LCJleHAiOjE3MDQxNDY3MTd9.Et_0TfcIF_xp7WM0LRFoip7eZ6MtUwxjh0ZJ-oVyPGs';
+
   private headers = new HttpHeaders({
-    Authorization: `Bearer ${this.tmpToken}`,
+    Authorization: `Bearer ${this.token}`,
   });
 
   private options = {
