2a788b1d844594f676f18a943a44d4397be429f5
rodis00 <happyekkov@gmail.com>
2024-01-02 20:46:42 +0100

    handle and send user profile data

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 import { UserProfileService } from '../services/userProfile/user-profile.service';
 import { TokenService } from '../services/token/token.service';
-import { UserProfile } from '../models/UserProfile';
+import { UserProfile, UserProfileWithId } from '../models/UserProfile';
 import { HttpErrorResponse } from '@angular/common/http';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
   providers: [UserProfileService, TokenService],
 })
 export class ProfileComponent {
-  userProfile?: UserProfile;
+  userProfileWithId!: UserProfileWithId;
 
   authToken: any = sessionStorage.getItem('token');
   token: any = {};
+
+  profileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    phoneNumber: new FormControl('', Validators.required),
+  });
 
   constructor(
     private userProfileService: UserProfileService,
@@ -30,13 +42,41 @@ export class ProfileComponent {
 
   private getProfile(): void {
     this.userProfileService.getUserProfile(this.token.id).subscribe(
-      (userProfile: UserProfile) => {
-        this.userProfile = userProfile;
-        console.log(this.userProfile);
+      (userProfile: UserProfileWithId) => {
+        this.userProfileWithId = userProfile;
+        console.log(this.userProfileWithId);
       },
       (error: HttpErrorResponse) => {
         console.log(error);
       }
     );
+  }
+
+  public handleSubmit(): void {
+    let formData = this.profileForm.value;
+    console.log(formData);
+    this.updateProfile(formData);
+  }
+
+  private updateProfile(profile: UserProfile): void {
+    this.userProfileService
+      .updateUserProfile(this.userProfileWithId.id, profile)
+      .subscribe(
+        (updatedProfile: UserProfileWithId) => {
+          this.userProfileWithId = updatedProfile;
+          this.refreshPage();
+          alert('Profil zaktualizowany.');
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+          alert('Błąd aktualizacji profilu.');
+        }
+      );
+  }
+
+  private refreshPage(): void {
+    setInterval(() => {
+      location.reload();
+    }, 300);
   }
 }
