4bf492f773cd51ac3d94b748fea6300430e03f72
rodis00 <happyekkov@gmail.com>
2024-01-02 18:10:40 +0100

    move userProfile fetch from parent to children component

--- frontend/src/app/account-page/account-page.component.ts
+++ frontend/src/app/account-page/account-page.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
-import { UserProfileService } from '../services/userProfile/user-profile.service';
-import { TokenService } from '../services/token/token.service';
-import { UserProfile } from '../models/UserProfile';
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-account-page',
@@ -11,7 +7,6 @@ import { HttpErrorResponse } from '@angular/common/http';
   imports: [ProfileComponent],
   templateUrl: './account-page.component.html',
   styleUrl: './account-page.component.css',
-  providers: [UserProfileService, TokenService],
 })
 export class AccountPageComponent {
   profile: string = 'Moje dane';
@@ -20,34 +15,7 @@ export class AccountPageComponent {
 
   selectedItem: string = this.profile;
 
-  userProfile?: UserProfile;
-
-  authToken: any = sessionStorage.getItem('token');
-  token: any = {};
-
-  constructor(
-    private userProfileService: UserProfileService,
-    private tokenService: TokenService
-  ) {
-    this.token = tokenService.getRolesAndIdFromToken(this.authToken);
-    if (this.token != null) {
-      this.getProfile();
-    }
-  }
-
   public selectIem(item: string): void {
     this.selectedItem = item;
-  }
-
-  private getProfile(): void {
-    this.userProfileService.getUserProfile(this.token.id).subscribe(
-      (userProfile: UserProfile) => {
-        this.userProfile = userProfile;
-        console.log(this.userProfile);
-      },
-      (error: HttpErrorResponse) => {
-        console.log(error);
-      }
-    );
   }
 }
--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { UserProfileService } from '../services/userProfile/user-profile.service';
+import { TokenService } from '../services/token/token.service';
+import { UserProfile } from '../models/UserProfile';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
+  providers: [UserProfileService, TokenService],
 })
 export class ProfileComponent {
+  userProfile?: UserProfile;
 
+  authToken: any = sessionStorage.getItem('token');
+  token: any = {};
+
+  constructor(
+    private userProfileService: UserProfileService,
+    private tokenService: TokenService
+  ) {
+    this.token = tokenService.getRolesAndIdFromToken(this.authToken);
+    if (this.token != null) {
+      this.getProfile();
+    }
+  }
+
+  private getProfile(): void {
+    this.userProfileService.getUserProfile(this.token.id).subscribe(
+      (userProfile: UserProfile) => {
+        this.userProfile = userProfile;
+        console.log(this.userProfile);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
 }
