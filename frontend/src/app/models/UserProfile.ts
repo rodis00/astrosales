0081c8d1e300cd625bc3f626bab2466ac81c41bf
rodis00 <happyekkov@gmail.com>
2024-01-02 20:44:36 +0100

    modyfiy userProfile interface, add new one

--- frontend/src/app/models/UserProfile.ts
+++ frontend/src/app/models/UserProfile.ts
@@ -1,5 +1,12 @@
-export interface UserProfile {
+export interface UserProfileWithId {
+  id: number;
   firstName: string;
   lastName: string;
   phoneNumber: string;
+}
+
+export interface UserProfile {
+  firstName?: string | null;
+  lastName?: string | null;
+  phoneNumber?: string | null;
 }
