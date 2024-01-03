1142bf71efddbec938ce2eaf6fc7e6366ce4ce32
rodis00 <happyekkov@gmail.com>
2024-01-03 16:23:01 +0100

    add ticket component

---
+++ frontend/src/app/ticket/ticket.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { FlightTransaction } from '../models/FlightTransaction';
+
+@Component({
+  selector: 'app-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket.component.html',
+  styleUrl: './ticket.component.css',
+})
+export class TicketComponent {
+  @Input() tickets!: FlightTransaction[];
+}
