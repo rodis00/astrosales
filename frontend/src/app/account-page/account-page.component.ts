b69ddddc2217675200a7db8778c9794aa8416b25
rodis00 <happyekkov@gmail.com>
2024-01-03 16:24:14 +0100

    implements ticket component

--- frontend/src/app/account-page/account-page.component.ts
+++ frontend/src/app/account-page/account-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
+import { ActiveTicketsComponent } from '../active-tickets/active-tickets.component';
+import { TicketsHistoryComponent } from '../tickets-history/tickets-history.component';
 
 @Component({
   selector: 'app-account-page',
   standalone: true,
-  imports: [ProfileComponent],
   templateUrl: './account-page.component.html',
   styleUrl: './account-page.component.css',
+  imports: [ProfileComponent, ActiveTicketsComponent, TicketsHistoryComponent],
 })
 export class AccountPageComponent {
   profile: string = 'Moje dane';
